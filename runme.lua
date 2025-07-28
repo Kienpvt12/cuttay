0569342b7cdd078d34d9ededb8b9c518f0578192
Kienpvt12 <153700448+Kienpvt12@users.noreply.github.com>
2025-07-29 03:25:10 +0700

    Update runme.lua

--- runme.lua
+++ runme.lua
@@ -31,6 +31,7 @@ getgenv().gagConfig = {
     SKIP_HARVEST_MUTATIONS = {},  -- Stop Harvest
 
     KEEP_PETS = { "Corrupted Kodama", "Kodama", "Corrupted Kitsune", "Raiju", ["Starfish"] = 4, ["Capybara"] = 1, ["Tanchozuru"] = 2, ["Seal"] = 1, "Kitsune", "Bald Eagle", "Fennec Fox", "Blood Kiwi", "Rooster", "Disco Bee", "Raccoon", "Queen Bee", "Dragonfly", "Butterfly", "Mimic Octopus", "Red Fox", "Chicken Zombie" },
+    KEEP_PETS_AGE = { ["Starfish"] = 75, ["Capybara"] = 75},
     KEEP_PETS_WEIGHT = 10,
 
     -- EQUIP_PETS Priority (Left -> Right)
