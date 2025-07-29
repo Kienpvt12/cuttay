96292952f5fe511191c0479b4cc03e673f7cda86
Kienpvt12 <153700448+Kienpvt12@users.noreply.github.com>
2025-07-30 00:17:05 +0700

    Update runme.lua

--- runme.lua
+++ runme.lua
@@ -4,7 +4,7 @@ getgenv().gagConfig = {
     CRAFT_EVENT = { "Anti Bee Egg"},
     BUY_TRAVELING_MERCHANT = { "Bee Egg", "Loquat", "Feijoa", "Pitcher Plant" },
     BUY_EVENT_SHOP = { "Zen Egg" },
-    MAX_EVENT_RESTOCK_SHECKLES = 500000000,
+    MAX_EVENT_RESTOCK_SHECKLES = 1900000000,
     PLACE_ALL_EVENT_STAFF = true,
     FOCUS_KITSUNE_NPC = true,
     
